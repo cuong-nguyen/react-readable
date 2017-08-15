1e12172c781440f657367154034a86d2ea471443
Cuong Nguyen <cuonghn8@live.com>
2017-08-15 15:45:44 +0700

    feat: Add React Router  - Add editorconfig file

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { Route, Switch } from 'react-router-dom'
+import Category from './Category'
+import Posts from './Posts'
+import PostDetails from './PostDetails'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path="/" component={Category} />
+					<Route path="/:categoryId/:postId" component={PostDetails} />
+					<Route path="/:categoryId" component={Posts} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/PostDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PostDetails extends Component {
+	render() {
+		return (
+			<div>Post Details</div>
+		)
+	}
+}
+
+export default PostDetails
