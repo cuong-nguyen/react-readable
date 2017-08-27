0e0c3060c664003bee87b7ecfb9b2da06564b864
Cuong Nguyen <cuonghn8@live.com>
2017-08-27 18:51:27 +0700

    fix: Sort post by voteScore initially

--- src/reducers/postFilter.js
+++ src/reducers/postFilter.js
@@ -1,7 +1,12 @@
 import { SORT_POST } from '../actions/actionTypes'
 import * as constants from '../constants'
 
-const postFilter = (state = {}, action) => {
+const initialFilter = {
+	sortBy: constants.SORT_BY_VOTES,
+	sortDir: constants.SORT_DESC
+}
+
+const postFilter = (state = initialFilter, action) => {
 	switch (action.type) {
 		case SORT_POST:
 			return {
