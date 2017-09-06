082adcb44d2ddca4741cdc39554b5ab6374a1437
Cuong Nguyen <cuonghn8@live.com>
2017-09-06 14:48:37 +0700

    feat: Add support for Redux state in Frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
-import { createStore } from 'redux'
 import rootReducer from '../reducers'
-import { applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { getCategories, fetchPosts } from '../actions'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const getStore = () => {
-  const store = createStore(rootReducer, applyMiddleware(thunk))
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  )
   store.dispatch(getCategories())
   store.dispatch(fetchPosts())
 
